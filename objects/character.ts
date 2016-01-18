3773f9f42f8f2304feb3aedb901a0e96ae3e8b6e
Trevor Baron <trevorjbaron@gmail.com>
2016-01-17 17:32:45 -0800

    fix rotation

--- objects/character.ts
+++ objects/character.ts
@@ -5,6 +5,7 @@ import Controller from "../objects/controller"
 class Character {
   body:THREE.Mesh
   spd:THREE.Vector3
+  view:THREE.Vector3 = new THREE.Vector3(0,0,0)
   moveAcc = 0.1;
   constructor(public controller:Controller, public collisionObjects:Array<THREE.Object3D>){
     this.body = new THREE.Mesh(new THREE.BoxGeometry(100, 100, 100), materials.toon);
@@ -22,9 +23,9 @@ class Character {
   getCameraPos(){
         var height = 50
         var ret = this.getCameraLook();
-        ret.x += Math.sin(this.body.rotation.y) * Math.cos(this.body.rotation.x)
-        ret.z += Math.cos(this.body.rotation.y) * Math.cos(this.body.rotation.x)
-        ret.y += Math.sin(this.body.rotation.x)
+        ret.x += Math.sin(this.view.y) * Math.cos(this.view.x)
+        ret.z += Math.cos(this.view.y) * Math.cos(this.view.x)
+        ret.y += Math.sin(this.view.x)
         var diff = ret.clone().sub(this.getCameraLook())
         return diff.multiplyScalar(1000).add(this.getCameraLook())
   }
@@ -37,13 +38,13 @@ class Character {
     this.xMouseVal = this.controller.getValue("rotX")* mouoseSpd
     var yChange = this.yMouseVal - this.controller.getValue("rotY")* mouoseSpd
     this.yMouseVal = this.controller.getValue("rotY")* mouoseSpd
-    this.body.rotation.x -= yChange
-    if(this.body.rotation.x  > Math.PI/2 - 0.1){
-      this.body.rotation.x = Math.PI/2 - 0.1
-    }else if(this.body.rotation.x  < -Math.PI/2 + 0.1){
-      this.body.rotation.x = -Math.PI/2 + 0.1
+    this.view.x -= yChange
+    if(this.view.x  > Math.PI/2 - 0.1){
+      this.view.x = Math.PI/2 - 0.1
+    }else if(this.view.x  < -Math.PI/2 + 0.1){
+      this.view.x = -Math.PI/2 + 0.1
     }
-    this.body.rotation.y += xChange
+    this.view.y += xChange
 
     //gravity
     this.spd.y -= 0.2
@@ -51,20 +52,20 @@ class Character {
     //key input
     var walkDir = new THREE.Vector3()
     if(this.controller.isDown("up")){
-      walkDir.x -= Math.sin(this.body.rotation.y) * this.moveAcc
-      walkDir.z -= Math.cos(this.body.rotation.y) * this.moveAcc
+      walkDir.x -= Math.sin(this.view.y) * this.moveAcc
+      walkDir.z -= Math.cos(this.view.y) * this.moveAcc
     }
     if(this.controller.isDown("down")){
-      walkDir.x += Math.sin(this.body.rotation.y) * this.moveAcc
-      walkDir.z += Math.cos(this.body.rotation.y) * this.moveAcc
+      walkDir.x += Math.sin(this.view.y) * this.moveAcc
+      walkDir.z += Math.cos(this.view.y) * this.moveAcc
     }
     if(this.controller.isDown("right")){
-      walkDir.z -= Math.sin(this.body.rotation.y) * this.moveAcc
-      walkDir.x += Math.cos(this.body.rotation.y) * this.moveAcc
+      walkDir.z -= Math.sin(this.view.y) * this.moveAcc
+      walkDir.x += Math.cos(this.view.y) * this.moveAcc
     }
     if(this.controller.isDown("left")){
-      walkDir.z += Math.sin(this.body.rotation.y) * this.moveAcc
-      walkDir.x -= Math.cos(this.body.rotation.y) * this.moveAcc
+      walkDir.z += Math.sin(this.view.y) * this.moveAcc
+      walkDir.x -= Math.cos(this.view.y) * this.moveAcc
     }
     if(this.controller.isDown("jump")){
       this.spd.y = 5
@@ -92,6 +93,7 @@ class Character {
 
 
     this.body.position.add(this.spd)
+    this.body.rotation.y = this.view.y
   }
 }
 
